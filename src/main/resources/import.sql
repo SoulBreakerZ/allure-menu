d71c034f0581cf6f2e41158943c1f842fea7c93f
Breaker <antivilo.trabajo@gmail.com>
2019-07-15 10:07:48 -0400

    Update import.sql
    Improve import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
 /* Populate table menu */
-INSERT INTO menu (name, path, description,creation_date,image_path) VALUES('ALLURE SGBATCH','/sg-batch', 'SITE FOR', NOW(),null);
-INSERT INTO menu (name, path, description,creation_date,image_path) VALUES('ALLURE SGADMIN','/sg-admin', 'SITE FOR', NOW(),null);
-INSERT INTO menu (name, path, description,creation_date,image_path) VALUES('ALLURE SGCSI','/sg-csi', 'SITE FOR', NOW(),null);
+INSERT INTO menu (name, path, description,creation_date,image_path) VALUES('SG-BATCH REPORT TEST SITE', '/sg-batch','Is reporting site build with allure-report', NOW(),null);
+INSERT INTO menu (name, path, description,creation_date,image_path) VALUES('SG-ADMIN REPORT TEST SITE', '/sg-admin','Is reporting site build with allure-report',NOW(),null);
+INSERT INTO menu (name, path, description,creation_date,image_path) VALUES('SG-CSI REPORT TEST SITE', '/sg-csi','Is reporting site build with allure-report',NOW(),null);
